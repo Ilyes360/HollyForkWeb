052852d7068cd4b64a4aaf243c1714d1d539e87a
Ilyes360 <127674783+Ilyes360@users.noreply.github.com>
2026-01-21 15:10:23 +0100

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.api_root, name='api-root'),
-    path('dashboard/', views.dashboard_data, name='dashboard-data'),
-]
-
